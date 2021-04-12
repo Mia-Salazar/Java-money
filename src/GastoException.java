d7d2390bd667b73738644e417935661772f6569f
Mia <maria.sgr@gmail.com>
2021-04-12 15:27:20 +0200

    Improve exception

--- src/GastoException.java
+++ src/GastoException.java
@@ -1,11 +1,5 @@
 public class GastoException extends Exception {
-	String sms;
-	 
 	public GastoException() {
-		this.sms = "No hay saldo suficiente";
+		super("No hay saldo suficiente");
 	}
-	 @Override
-	 public String getMessage (){
-		 return sms;
-	 }
 }
