505fa9e896b9e93801a0ed2f3afbf6d438936f1c
Mia <maria.sgr@gmail.com>
2021-04-15 11:12:42 +0200

    Add \n

--- src/GastoException.java
+++ src/GastoException.java
@@ -1,6 +1,6 @@
 public class GastoException extends Exception {
 	public GastoException() {
 		//Devolvemos el mensaje de error
-		super("No hay saldo suficiente");
+		super("No hay saldo suficiente\n");
 	}
 }
